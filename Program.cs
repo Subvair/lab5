644b66f79f0f19ccad44b6cf0256a9dbb1840dcc
subvair <vadimogienko42k@gmail.com>
2025-04-04 00:15:18 +0700

    Добавил словарь

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 class Program {
-    static void Main() {
-
+  static void Main() {
+    
     }
 }
--- TextProcessor.cs
+++ TextProcessor.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
-class TextProcessor{
+class TextProcessor {
+  private readonly Dictionary<string, string> mistakeDictionary = new Dictionary<string, string> {
+    { "првиет", "привет" },
+    { "пирвет", "привет" },
+    { "првт", "привет" },
+    { "пок", "пока" },
+    { "пка", "пока" },
+    { "ппка", "пока" }
+    };
     
 }
