694eb8553963ac4930982c551502d52386077370
subvair <vadimogienko42k@gmail.com>
2025-04-04 00:34:13 +0700

    Дописал main

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 
 class Program {
   static void Main() {
-    
+    Console.Write("Введите путь к папке с текстами: ");
+    string directoryPath = Console.ReadLine();
+
+    TextProcessor processor = new TextProcessor();
+    processor.ProcessDirectory(directoryPath);
+
+    Console.WriteLine("Готово!");
     }
 }
